3ca57453a56c784f2d1ca0a4154a209246fb8bc4
Andrii Doroshev <ostinred@gmail.com>
2020-04-07 17:33:29 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  var $body = $('body');
+  var $header = $('.is-header');
+  var $navBtn = $('#navBtn');
+  var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+  var isChrome = /Chrome/i.test(navigator.userAgent);
+  var mq = $(window).width() < 1023;
+  var $main = $('.main');
+
+  $navBtn.click(function() {
+    $header.toggleClass('is-open');
+    $body.toggleClass('is-fixed');
+  });
+
+  function detectBrowser() {
+    if (isSafari) {
+      $body.addClass('isIos');
+    } else if (isChrome && mq) {
+      $body.addClass('isChrome');
+    }
+  }
+  detectBrowser();
+
+  function fullHeightBanner() {
+    if ($body.hasClass('isChrome')) {
+      $main.css({ height: window.innerHeight });
+    }
+  }
+  fullHeightBanner();
+});
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate(
+    {
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    },
+    500
+  );
+});
